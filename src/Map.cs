5b0276b2518781b57f88b837543c7037d40c5deb
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-24 14:42:18 +0200

    create class map

---
+++ src/Map.cs
@@ -0,0 +1,15 @@
+﻿using Position = Dungeon_Crawl.src.Objects.Position;
+
+namespace Dungeon_Crawl.src;
+internal class Map
+{
+    static void DisplayCurrentMap()
+    {
+
+    }
+
+    public GameObject At(Position position)
+    {
+        return
+    }
+}
