fae3ba5a4ca5d168417dc8bbc9345627102f5894
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-24 14:18:17 +0200

    create folder for utils

---
+++ src/Utils/Stats.cs
@@ -0,0 +1,9 @@
+﻿namespace Dungeon_Crawl.src.Utils;
+
+internal class Stats
+{
+    internal int HealthPoints { get; set; }
+    internal int Strength  { get; set; }
+    internal static int Defense { get; set; }
+}
+
