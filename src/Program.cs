981ff019b7355b0f798f3208bb67cba449e89c81
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-24 14:17:38 +0200

    refactor

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ungeon_Crawl.src;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+    }
+}
