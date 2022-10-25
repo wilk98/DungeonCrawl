6bdea7164c7e62a76bf6b8965237c7ed5680e497
Andrii Danylkov <danilkovxp@gmail.com>
2022-10-25 10:53:20 +0200

    Extract interface for movable objects

--- src/Player.cs
+++ src/Player.cs
@@ -1,6 +1,24 @@
 ﻿namespace Dungeon_Crawl.src;
 
-internal class Player
+internal class Player : DynamicObject, IMovable
 {
+    public Player(Position position) : base(position)
+    {
+    }
+
+    public void ProcessInput(char key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Render()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new NotImplementedException();
+    }
 }
 
---
+++ src/Utils/IMovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawl.src.Utils
+{
+    internal interface IMovable
+    {
+        public abstract void ProcessInput(char key);
+    }
+}
