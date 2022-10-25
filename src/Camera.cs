108b41ae08758aa2c95d557e59b793caa5f9e13c
Andrii Danylkov <danilkovxp@gmail.com>
2022-10-25 14:14:59 +0200

    Add camera class that projects view on map depending on position

---
+++ src/Camera.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawl.src
+{
+    internal class Camera
+    {
+        private const int _width = 81;
+        private const int _height = 25;
+
+        public List<string> GetView(Position centralPosition, Map map)
+        {
+            var mapView = map.RenderMap();
+
+            var startHeight = Math.Max(0, centralPosition.Y - (_height - 1) / 2);
+            var endHeight = Math.Min(startHeight + _height - 1, map.Height - 1);
+
+            Debug.Assert(startHeight < endHeight, "początkowa wysokość powinna być mniejsz od końcowej");
+
+            var startWidth = Math.Max(0, centralPosition.X - (_width - 1) / 2);
+            var endWidth = Math.Min(startWidth + _width - 1, map.Width - 1);
+
+            Debug.Assert(startWidth < endWidth);
+
+            var cameraView = new List<string>();
+            var verticalRange = mapView.ToArray()[startHeight..endHeight];
+            foreach (var line in verticalRange)
+            {
+                cameraView.Add(line[startWidth..endWidth]);
+            }
+            return cameraView;
+        }
+    }
+}
