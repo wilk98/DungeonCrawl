0e4db4dcce23f0d90395ef43fe26b0d09cc8c5ab
Andrii Danylkov <danilkovxp@gmail.com>
2022-10-25 18:53:36 +0200

    Add movement controller

---
+++ src/Actions/Movement.cs
@@ -0,0 +1,28 @@
+﻿using Dungeon_Crawl.src.Core;
+using System.Runtime.CompilerServices;
+
+namespace Dungeon_Crawl.src.Actions
+{
+    internal class Movement
+    {
+        private Map _map;
+        public Movement(Map map)
+        {
+            _map = map;
+        }
+        private bool IsValidMove(Position positionTo)
+        {
+            var destination = _map.At(positionTo);
+            return destination.IsPassable;
+        }
+        public bool Move(Position positionFrom, Position positionTo)
+        {
+            if (!IsValidMove(positionTo))
+                return false;
+
+            _map.ChangePositions(positionFrom, positionTo);
+            return true;
+        }
+
+    }
+}
