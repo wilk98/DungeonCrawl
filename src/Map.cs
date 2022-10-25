a24a4ff27406fbbc7b3ed0dee8542b3be8a2630f
Andrii Danylkov <danilkovxp@gmail.com>
2022-10-25 11:01:48 +0200

    Add Map render

--- src/Map.cs
+++ src/Map.cs
@@ -1,15 +1,63 @@
-﻿using Position = Dungeon_Crawl.src.Objects.Position;
+﻿using Dungeon_Crawl.src.Objects.StaticObjects;
 
 namespace Dungeon_Crawl.src;
 internal class Map
 {
-    static void DisplayCurrentMap()
-    {
+    private readonly GameObject[,] _field;
+    private const int _width = 243;
+    private const int _height = 75;
 
+    public Map()
+    {
+        _field = new GameObject[_width, _height];
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                _field[x, y] = new Wall(new Position(x, y));
+            }
+        }
+    }
+
+    public List<string> RenderMap()
+    {
+        Update();
+        Render();
+        var map = new List<string>();
+        for (int x = 0; x < _width; x++)
+        {
+            var row = new string[_width];
+            for (int y = 0; y < _height; y++)
+            {
+                row[x] = At(new Position(x, y)).ToString();
+            }
+            map.Add(string.Join("", row));
+        }
+        return map;
+    }
+
+    private void Update()
+    {
+        foreach (var gameObject in _field)
+        {
+            gameObject.Update();
+        }
+    }
+
+    private void Render()
+    {
+        foreach (var gameObject in _field)
+        {
+            gameObject.Render();
+        }
     }
 
     public GameObject At(Position position)
     {
-        return
+        if (position.X < 0 || position.X > _width)
+            throw new ArgumentOutOfRangeException();
+        if (position.Y < 0 || position.Y > _height)
+            throw new ArgumentOutOfRangeException();
+        return _field[position.X, position.Y];
     }
 }
