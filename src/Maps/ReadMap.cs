be15baa58fcb25e61d1370d47da9b20b2f6680df
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-26 14:16:55 +0200

    first map almost works

--- src/Maps/ReadMap.cs
+++ src/Maps/ReadMap.cs
@@ -1,29 +1,11 @@
-﻿using Dungeon_Crawl.src.Core;
-
-namespace Dungeon_Crawl.src.Maps;
+﻿namespace Dungeon_Crawl.src.Maps;
 internal class ReadMap
     {
-        private static GameObject[,] _field;
-        private static int _width = 67;
-        private static int _height = 151;
-        private static int count = 0;
-        public static int Width => _width;
 
-        public static int Height => _height;
-    public static void ReadFirstMapFile()
+    public static string[] ReadFirstMapFile()
         {
-            _field = new GameObject[Width, Height];
             var firstMap = File.ReadAllLines(@"..\..\..\src\Maps\map1.txt");
-            for (int i = 0; i < firstMap.Length; i++)
-            {
-                for (int j = 0; j < firstMap[i].Length; j++)
-                {
-                    if (firstMap[i][j] == '#') 
-                    {
-                        _field[i, j] = new Wall(new Position(i, j));
-                    }
-                }
-            }
+            return firstMap;
         }
     }
     
---
+++ src/Objects/StaticObjects/NPC.cs
@@ -0,0 +1,10 @@
+﻿namespace Dungeon_Crawl.src.Objects.StaticObjects;
+internal class NPC : StaticObject
+{
+    public NPC(Position position) : base(position)
+    {
+    }
+
+    protected override string Symbol { get => "N"; set => throw new NotImplementedException(); }
+}
+
