81e9f02757899cf8f5f0031b16a34c3ecd257c57
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-26 11:30:24 +0200

    test

---
+++ src/Maps/ReadMap.cs
@@ -0,0 +1,25 @@
+﻿namespace Dungeon_Crawl.src.Maps;
+internal class ReadMap
+    {
+        private static GameObject[,] _field;
+        private static int _width = 151;
+        private static int _height = 67;
+        private static int count = 0;
+        public static int Width => _width;
+
+        public static int Height => _height;
+    public static void ReadFirstMapFile()
+        {
+            _field = new GameObject[Width, Height];
+            var firstMap = File.ReadAllLines(@"..\..\..\src\Maps\map1.txt");
+            for (int i = 0; i < firstMap.Length; i++)
+            {
+                for (int j = 0; j < firstMap[i].Length; j++)
+                {
+                    _field[i,j] = firstMap[i][j];
+                }
+            }   
+        }
+        }
+    
+
--- src/Utils/Usings.cs
+++ src/Utils/Usings.cs
@@ -3,3 +3,4 @@ global using Dungeon_Crawl.src.StaticObjects;
 global using Dungeon_Crawl.src.Utils;
 global using System;
 global using System.Collections.Generic;
+global using Dungeon_Crawl.src.Maps;
