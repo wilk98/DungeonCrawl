65922710a8f407d384897cf8fee38ca9824a35b2
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-27 11:08:37 +0200

    move usings

---
+++ src/Objects/StaticObjects/ItemList.cs
@@ -0,0 +1,29 @@
+﻿namespace Dungeon_Crawl.src.Objects.StaticObjects;
+internal class ItemList
+{
+    private List<Item> Items;
+
+    public ItemList(Position position, Map map)
+    {
+        Items = new List<Item>()
+        {
+            new Item("Sword", position, map),
+            new Item("Armor", position, map),
+            new Item("Shield", position, map),
+            new Item("Helmet", position, map),
+            new Item("Greaves", position, map),
+            new Item("Health potion", position, map),
+            new Item("Strength potion", position, map),
+            new Item("Defense potion", position, map),
+        };
+    }
+
+    public Item RandomItem()
+    {
+        var random = new Random();
+        var itemIndex = random.Next(Items.Count);
+        var item = Items[itemIndex];
+        return item;
+    }
+}
+
