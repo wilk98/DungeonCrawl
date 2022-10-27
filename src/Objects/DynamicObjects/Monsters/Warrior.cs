797731f453e0a98e0698beba8058c819baac609b
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-27 11:55:26 +0200

    create monster warrior type

--- src/Objects/DynamicObjects/Monsters/Warrior.cs
+++ src/Objects/DynamicObjects/Monsters/Warrior.cs
@@ -1,6 +1,21 @@
 ﻿namespace Dungeon_Crawl.src.Monsters;
 
-internal class Warrior
+internal class Warrior : Monster
 {
+    public Warrior(Position position) : base(position)
+    {
+    }
+
+    protected override string Symbol { get => "W"; set => throw new NotImplementedException(); }
+
+    public override void Update()
+    {
+        return;
+    }
+
+    public override string Render()
+    {
+        return "";
+    }
 }
 
