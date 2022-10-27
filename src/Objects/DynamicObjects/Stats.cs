be1c7b06edcbe5861ffc8f88e5bdabcbd59f429d
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-27 16:43:01 +0200

    create method to update stats by items

--- src/Objects/DynamicObjects/Stats.cs
+++ src/Objects/DynamicObjects/Stats.cs
@@ -5,5 +5,11 @@ internal class Stats
     internal int HealthPoints { get; set; }
     internal int Strength { get; set; }
     internal int Defense { get; set; }
-}
 
+    public void UpdateStats(int healthPoints, int strength, int defense)
+    {
+        HealthPoints += healthPoints;
+        Strength += strength;
+        Defense += defense;
+    }
+}
