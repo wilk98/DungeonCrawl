a76a063eb7fed1c998745c49d1854ea645fc21a2
Ainsik <pio.andrzejczuk@gmail.com>
2022-10-27 11:56:17 +0200

    create monster list class and pick random monster

---
+++ src/Objects/DynamicObjects/Monsters/MonsterList.cs
@@ -0,0 +1,23 @@
+﻿namespace Dungeon_Crawl.src.Objects.DynamicObjects.Monsters;
+internal class MonsterList
+    {
+        private List<Monster> Monsters;
+
+        public MonsterList(Position position)
+        {
+            Monsters = new List<Monster>()
+            {
+                new Archer(position),
+                new Warrior(position)
+            };
+        }
+
+        public Monster RandomMonster()
+        {
+            var random = new Random();
+            var monsterIndex = random.Next(Monsters.Count);
+            var monster = Monsters[monsterIndex];
+            return monster;
+        }
+}
+
