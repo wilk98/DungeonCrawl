2a2a18dde506b1ac3f6340a598636aed2d68305b
Andrii Danylkov <danilkovxp@gmail.com>
2022-10-28 09:34:25 +0200

    Add simple menu

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,9 @@ public class Program
 
     public static void Main(string[] args)
     {
+        var menu = new Menu();
+        menu.PrintMenu();
+        menu.ChoiceFromMenu(Console.ReadKey().Key);
         var game = new Game();
         game.Start();
     }
