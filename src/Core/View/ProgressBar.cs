2f55ef1a5fd6ea65ebb8e365d75093a7aba65449
wilk98 <maciekwilk09@gmail.com>
2022-11-08 01:34:56 +0100

    display bar of health and experience

---
+++ src/Core/View/ProgressBar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawl.src.Core.View
+{
+    public class ProgressBar
+    {
+        public string block = "■";
+
+        public int CountBlock(int currentValue, int maxValue)
+        {
+            int maxBlockQuantity = 28;
+            int currentBlockQuantity = maxBlockQuantity * currentValue / maxValue;
+            return currentBlockQuantity;
+        }
+
+        public string DrawBar(int currentValue, int maxValue)
+        {
+            int numberOfBlocks = CountBlock(currentValue, maxValue);
+            string bar = new String('■', numberOfBlocks);
+            return bar;
+        }
+    }
+}
