bae843e871035026481abf5b0b8a6141cefcbd05
Andrii Danylkov <danilkovxp@gmail.com>
2022-11-08 20:22:06 +0100

    Add rarity to items

--- src/Objects/StaticObjects/Items/Armor.cs
+++ src/Objects/StaticObjects/Items/Armor.cs
@@ -1,13 +1,31 @@
 ﻿namespace Dungeon_Crawl.src.Objects.StaticObjects.Items;
 public abstract class Armor : Item
 {
-    public Armor(Position position, Map map) : base(position, map)
+    private readonly Rarity _rarity;
+
+    public Armor(Position position, Map map, Rarity rarity) : base(position, map)
     {
-        //Stats.Strength = 0;
-        Stats.Defense = 4;
-        //Stats.HealthPoints = 0;
+        _rarity = rarity;
+
+        Stats.HealthPoints = (int) Math.Round(GetRarityMultiplier() * Stats.HealthPoints);
+        Stats.Strength = (int) Math.Round(GetRarityMultiplier() * Stats.Strength);
+        Stats.Defense = (int) Math.Round(GetRarityMultiplier() * Stats.Defense);
     }
 
+
+    public override string Name => $"{_rarity} ";
+    private float GetRarityMultiplier()
+    {
+        return _rarity switch
+        {
+            Rarity.Normal => 1.0f,
+            Rarity.Common => 1.25f,
+            Rarity.Rare => 1.5f,
+            Rarity.Unique => 2.0f,
+            Rarity.Legendary => 3.0f,
+            _ => throw new NotImplementedException(),
+        };
+    }
     public abstract EquipmentType equipmentType { get; }
     public override State Use(Player player)
     {
