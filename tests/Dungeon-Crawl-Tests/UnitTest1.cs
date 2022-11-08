a64ce5448d91161ab7ff7d85a0fc2b79deaa128e
Andrii Danylkov <danilkovxp@gmail.com>
2022-11-08 13:33:07 +0100

    Add tests to project

---
+++ tests/Dungeon-Crawl-Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Dungeon_Crawl_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
