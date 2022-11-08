d8d9ed854ec86bcc2e18aee542574a4b0e6f231b
Andrii Danylkov <danilkovxp@gmail.com>
2022-11-08 20:48:48 +0100

    Add test for sword

--- src/Objects/StaticObjects/Items/Greaves.cs
+++ src/Objects/StaticObjects/Items/Greaves.cs
@@ -1,11 +1,10 @@
 ﻿namespace Dungeon_Crawl.src.Objects.StaticObjects.Items;
-internal class Greaves : Armor
+public class Greaves : Armor
 {
     public Greaves(Position position, Map map, Rarity rarity = Rarity.Common) : base(position, map, rarity)
     {
-        //Stats.Strength = 0;
         Stats.Defense = 2;
-        //Stats.HealthPoints = 0;
+        ApplyRarityMultiplierToStats();
     }
     public override string Name => base.Name + "Greaves";
 
