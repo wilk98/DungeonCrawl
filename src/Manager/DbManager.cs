bc2c83015b444ffee21991f5c3bfe4e4c13b081e
Andrii Danylkov <danilkovxp@gmail.com>
2022-11-09 10:46:20 +0100

    Add DB manager to game

---
+++ src/Manager/DbManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Dungeon_Crawl.src.Manager
+{
+    public class DbManager
+    {
+        public string ConnectionString => ConfigurationManager.AppSettings["connectionString"];
+        public bool TestConnection()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open(); return true;
+                }
+                catch (Exception e)
+                { return false; }
+            }
+        }
+    }
+}
