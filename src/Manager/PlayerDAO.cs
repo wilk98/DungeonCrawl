b00a10d998305734cc95699f6f8e4182e4ea912d
Andrii Danylkov <danilkovxp@gmail.com>
2022-11-09 13:26:47 +0100

    Add tests for saving player

--- src/Manager/PlayerDAO.cs
+++ src/Manager/PlayerDAO.cs
@@ -8,10 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 using System.Xml.Serialization;
+using System.Xml;
+using Dungeon_Crawl.src.Objects.DynamicObjects;
 
 namespace Dungeon_Crawl.src.Manager
 {
-    internal class PlayerDao : IPlayerDao
+    public class PlayerDao : IPlayerDao
     {
         private readonly string _connectionString;
         private const string DateFormat = "yyyy-MM-dd";
@@ -20,16 +22,55 @@ namespace Dungeon_Crawl.src.Manager
             _connectionString = connectionString;
         }
 
-        private static string SerializeToXml<T>(T value)
+        public string SerializeToXml<T>(T value)
         {
             StringWriter writer = new StringWriter(CultureInfo.InvariantCulture);
             XmlSerializer serializer = new XmlSerializer(typeof(T));
             serializer.Serialize(writer, value);
             return writer.ToString();
         }
+        public T XmlDeserializeFromString<T>(string objectData)
+        {
+            return (T)XmlDeserializeFromString(objectData, typeof(T));
+        }
 
+        private static object XmlDeserializeFromString( string objectData, Type type)
+        {
+            var serializer = new XmlSerializer(type);
+            object result;
+
+            using (TextReader reader = new StringReader(objectData))
+            {
+                result = serializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+
+        public void ClearTable()
+        {
+            const string insertCommand = @"DELETE FROM player";
+
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var cmd = new SqlCommand(insertCommand, connection);
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+                    var a =cmd.BeginExecuteNonQuery();
+                    cmd.EndExecuteNonQuery(a);
+                    connection.Close();
+                }
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+        }
         public void Add(Player player)
         {
+            ClearTable();
             const string insertCommand = @"INSERT INTO player (stats, position, level, items)
                         VALUES (@stats, @position, @level, @items)
                         SELECT SCOPE_IDENTITY();";
@@ -45,8 +86,8 @@ namespace Dungeon_Crawl.src.Manager
                     cmd.Parameters.AddWithValue("@position", SerializeToXml(player.Position));
                     cmd.Parameters.AddWithValue("@level", SerializeToXml(player.Level));
                     cmd.Parameters.AddWithValue("@items", SerializeToXml(player.Inventory.GetItems()));
-                    
-                    cmd.BeginExecuteNonQuery();
+
+                    cmd.ExecuteNonQuery();
                     connection.Close();
                 }
             }
@@ -55,14 +96,45 @@ namespace Dungeon_Crawl.src.Manager
                 throw e;
             }
         }
-
-        public Player Get(int id)
-        {
-            throw new NotImplementedException();
-        }
         public void Update(Player player)
         {
             throw new NotImplementedException();
+        }
+
+        public Player Get(Map map)
+        {
+            const string cmdText = @"SELECT TOP 1 *
+                                     FROM [dungeoun-crawl].[dbo].[player]";
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var cmd = new SqlCommand(cmdText, connection);
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+
+                    var reader = cmd.ExecuteReader();
+                    // TODO: extract Author from result set
+
+                    reader.Read();
+                    var stats = reader.GetString("stats");
+                    var position = reader.GetString("position");
+                    var level = reader.GetString("level");
+                    var items = reader.GetString("items");
+
+                    connection.Close();
+                    var positionObj = XmlDeserializeFromString<Position>(position);
+                    var player = new Player(positionObj, new Movement(map), map);
+                    player.Level = XmlDeserializeFromString<LevelUp>(level);
+                    player.Stats = XmlDeserializeFromString<Stats>(stats);
+
+                    return player;
+                }
+            }
+            catch (SqlException e)
+            {
+                throw new RuntimeWrappedException(e);
+            }
         }
     }
 }
